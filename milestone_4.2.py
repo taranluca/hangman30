aec70722dae7363006a432f8a3b2b75630da3beb
Taran Luca Patel <taranlucapatel@gmail.com>
2024-07-23 18:16:51 +0100

    Made some corrections to milestone_4 while editing milestone_5

--- milestone_4.2.py
+++ milestone_4.2.py
@@ -3,10 +3,10 @@ import random
 class Hangman:
     def __init__(self,word,word_guessed,num_letters,list_of_guesses):
         self.word = word #Currently being called through usign the Fruit Variable
+        self.word_guessed = word_guessed
         self.num_letters = num_letters
         self.list_of_guesses = list_of_guesses
-        self.word_guessed = word_guessed
-        self.word_list = ["mango", "apple", "pear", "grape", "orange"]
+        self.word_list = ["mango", "apple", "pear", "grape", "orange","banana"]
         self.num_lives = 5
 
     def check_guess(self,guess):
@@ -14,11 +14,11 @@ class Hangman:
         if guess in self.word:
             print("")
             print(f"Well done, '{guess}', is in the word")
-            print(self.list_of_guesses)
+            print(f"Letters guessed already: {self.list_of_guesses}")
             print("")
             for index, character in enumerate(empty_list):
                 if guess == character:
-                    word_list[index] = guess
+                    word_guessed[index] = guess
             self.num_letters = self.num_letters - 1
         else:
             print(f"Sorry, '{guess}', is not in the word")
@@ -30,7 +30,7 @@ class Hangman:
             print(f"Number of Unique Letters Remaining {self.num_letters}")
             print(f"Number of Lives Remaining {self.num_lives}")
             print("")
-            print(word_list)
+            print(word_guessed)
             guess = input("Enter a Single Letter ")
             #print(guess)
             if guess.isalpha() == False or len(guess) > 1:
@@ -43,14 +43,16 @@ class Hangman:
                 self.list_of_guesses.append(guess)
                 self.check_guess(guess)
 
-Fruit = "Banana" #Input any friut
-num_letters = len(set(Fruit))
-Fruit = Fruit.lower() #ensures the string is all lower case
+word_list = ["mango", "apple", "pear", "grape", "orange","banana"] #Input any friut
+word = random.choice(word_list)
+print(word)
+num_letters = len(set(word))
+word = word.lower() #ensures the string is all lower case
 empty_list = [] #Use to store each character of Fruits in a list
-word_list = [] #Used to show correct letters ["_","_",etc.]
-for character in Fruit:
+word_guessed = [] #Used to show correct letters ["_","_",etc.]
+for character in word:
   empty_list.append(character) #separates characters into elements in the list
-  word_list.append("_") #assigns the number of spaces needed
+  word_guessed.append("_") #assigns the number of spaces needed
 
-Test_word = Hangman(Fruit,word_list,num_letters,[])
+Test_word = Hangman(word,word_guessed,num_letters,[])
 Test_word.ask_for_input()
